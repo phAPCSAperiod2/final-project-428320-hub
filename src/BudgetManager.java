0bd58723fcf663e94e162bf18cafe64103902fee
428320-hub <428320@stu.sandi.net>
2026-04-15 16:56:02 +0000

    commit 2

--- src/BudgetManager.java
+++ src/BudgetManager.java
@@ -1,11 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale.Category;
+import java.util.stream.Collectors;
 
 public class BudgetManager {
-    // use a List to store all our Transaction objects
     private List<Transaction> transactions;
 
     public BudgetManager() {
         this.transactions = new ArrayList<>();
     }
 
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+    }
+
+    public List<Transaction> getTransactionsByCategory(Category category) {
+        return transactions.stream()
+                .filter(t -> t.getCategory() == category)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/Transaction.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.util.Locale.Category;
+
+public class Transaction {
+    private double amount;
+    private Category category;
+    private String description;
+    private LocalDate date;
+
